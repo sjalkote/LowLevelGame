9ac6bbab39f10d1869f88dd2ed1330cc20069b60
TechnoShip123 <75491816+TechnoShip123@users.noreply.github.com>
2023-10-20 17:42:06 -0500

    :broom: Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,20 @@ int SCREEN_WIDTH = 1920 / 2;
 int SCREEN_HEIGHT = 1080 / 2;
 
 // Window and Renderer
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
 
 void update() {
-    const Uint8* keystates = SDL_GetKeyboardState(NULL);
-    if (keystates[SDL_SCANCODE_UP] || keystates[SDL_SCANCODE_W]) {          // UP
+    const Uint8* keyPressed = SDL_GetKeyboardState(nullptr);
+    if (keyPressed[SDL_SCANCODE_UP] || keyPressed[SDL_SCANCODE_W]) {          // UP
         cout << "UP" << endl;
-    } else if (keystates[SDL_SCANCODE_DOWN] || keystates[SDL_SCANCODE_S]) { // DOWN
+    } else if (keyPressed[SDL_SCANCODE_DOWN] || keyPressed[SDL_SCANCODE_S]) { // DOWN
         cout << "DOWN" << endl;
-    } else if (keystates[SDL_SCANCODE_LEFT] || keystates[SDL_SCANCODE_A]) { // LEFT
+    } else if (keyPressed[SDL_SCANCODE_LEFT] || keyPressed[SDL_SCANCODE_A]) { // LEFT
         cout << "LEFT" << endl;
-    } else if (keystates[SDL_SCANCODE_RIGHT] || keystates[SDL_SCANCODE_D]) {// RIGHT
+    } else if (keyPressed[SDL_SCANCODE_RIGHT] || keyPressed[SDL_SCANCODE_D]) {// RIGHT
         cout << "RIGHT" << endl;
-    } else if (keystates[SDL_SCANCODE_SPACE]) {                             // JUMP
+    } else if (keyPressed[SDL_SCANCODE_SPACE]) {                              // JUMP
         cout << "JUMP?" << endl;
     }
 
@@ -64,7 +64,7 @@ int main(int argc, char* args[]) {
 
 #pragma region Initialize SDL, window, renderer, etc.
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-        cout << "Error initializing SDL: " << SDL_GetError() << endl; return EXIT_FAILURE;
+        std::cerr << "Error initializing SDL: " << SDL_GetError() << endl; return EXIT_FAILURE;
     }
     cout << "SDL init successful.\n";
 
@@ -77,15 +77,15 @@ int main(int argc, char* args[]) {
         SDL_WINDOW_RESIZABLE
     );
 
-    if (window == NULL) {
-        cout << "Error creating window: " << SDL_GetError() << endl; return EXIT_FAILURE;
+    if (window == nullptr) {
+        std::cerr << "Error creating window: " << SDL_GetError() << endl; return EXIT_FAILURE;
     }
     SDL_SetWindowTitle(window, "LowLevelGame");
 
     // Create renderer
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == NULL) {
-        cout << "Error creating renderer: " << SDL_GetError() << endl; return EXIT_FAILURE;
+    if (renderer == nullptr) {
+        std::cerr << "Error creating renderer: " << SDL_GetError() << endl; return EXIT_FAILURE;
     }
 
     cout << "Window and renderer init successful.\n";
@@ -103,8 +103,8 @@ int main(int argc, char* args[]) {
     
     // TODO: Move exit tasks to their own Quit() function?
     cout << "Exiting..." << endl;
-    SDL_DestroyRenderer(renderer); renderer = NULL;
-    SDL_DestroyWindow(window); window = NULL;
+    SDL_DestroyRenderer(renderer); renderer = nullptr;
+    SDL_DestroyWindow(window); window = nullptr;
     SDL_Quit();
     return EXIT_SUCCESS;
 }
